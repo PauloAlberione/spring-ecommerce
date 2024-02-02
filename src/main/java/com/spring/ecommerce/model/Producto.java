ba38d33dbbb887396c76f4b420a774c79b9ae874
PauloAlberione <pauloalberione@gmail.com>
2024-02-02 20:13:43 -0300

    Crear las clases model y el mapeo de clases a trablas con JPA, integración de diseño y vistas

--- src/main/java/com/spring/ecommerce/model/Producto.java
+++ src/main/java/com/spring/ecommerce/model/Producto.java
@@ -1,7 +1,18 @@
 package com.spring.ecommerce.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Productos")
 public class Producto {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nombre;
 	private String descripcion;
@@ -9,11 +20,15 @@ public class Producto {
 	private double precio;
 	private int cantidad;
 	
+	@ManyToOne
+	private Usuario usuario;
+	
 	public Producto() {
 		
 	}
-
-	public Producto(Integer id, String nombre, String descripcion, String imagen, double precio, int cantidad) {
+	
+	public Producto(Integer id, String nombre, String descripcion, String imagen, double precio, int cantidad,
+			Usuario usuario) {
 		super();
 		this.id = id;
 		this.nombre = nombre;
@@ -21,6 +36,7 @@ public class Producto {
 		this.imagen = imagen;
 		this.precio = precio;
 		this.cantidad = cantidad;
+		this.usuario = usuario;
 	}
 
 	public Integer getId() {
@@ -70,7 +86,15 @@ public class Producto {
 	public void setCantidad(int cantidad) {
 		this.cantidad = cantidad;
 	}
+	
+	public Usuario getUsuario() {
+		return usuario;
+	}
 
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
 	@Override
 	public String toString() {
 		return "Producto [id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", imagen=" + imagen
