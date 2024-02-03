412ecc50cac6984536ac0b813d2cad1977f3e1e7
PauloAlberione <pauloalberione@gmail.com>
2024-02-03 00:40:28 -0300

    Funcionalidad para mostrar los productos al Administrador

--- src/main/java/com/spring/ecommerce/service/ProductoService.java
+++ src/main/java/com/spring/ecommerce/service/ProductoService.java
@@ -1,5 +1,6 @@
 package com.spring.ecommerce.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.spring.ecommerce.model.Producto;
@@ -7,10 +8,8 @@ import com.spring.ecommerce.model.Producto;
 public interface ProductoService {
 
 	public Producto save(Producto producto);
-	
 	public Optional<Producto> get(Integer id);
-	
 	public void update(Producto producto);
-	
 	public void delete(Integer id);
+	public List<Producto> findAll();
 }
