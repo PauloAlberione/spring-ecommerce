44a4652010c3ee4293292c01301c4c1b5e438a2d
PauloAlberione <pauloalberione@gmail.com>
2024-02-06 02:16:18 -0300

    Funcionalidad pra que el administrador vea los Usuarios del Ecommerce

--- src/main/java/com/spring/ecommerce/controller/AdministradorController.java
+++ src/main/java/com/spring/ecommerce/controller/AdministradorController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.spring.ecommerce.model.Producto;
+import com.spring.ecommerce.service.IUsuarioService;
 import com.spring.ecommerce.service.ProductoService;
 
 @Controller
@@ -18,6 +19,9 @@ public class AdministradorController {
 	@Autowired
 	private ProductoService productoService;
 	
+	@Autowired
+	private IUsuarioService usuarioService;
+	
 	@GetMapping("")
 	public String home(Model model) {
 		
@@ -25,5 +29,12 @@ public class AdministradorController {
 		model.addAttribute("productos", productos);
 		
 		return "administrador/home";
+	}
+	
+	@GetMapping("/usuarios")
+	public String usuarios(Model model) {
+		
+		model.addAttribute("usuarios", usuarioService.findAll());
+		return "administrador/usuarios";
 	}
 }
